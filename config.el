b26edc51aaf64453ef86f9cce21b378b4a0a8f6d
tvirolai <tuomo.virolainen@siili.com>
2021-12-30 21:13:39 +0200

    Set the font config debacle straight

--- config.el
+++ config.el
@@ -19,13 +19,18 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-; (setq doom-font "SF Mono-13"
-;       doom-variable-pitch-font "Input Mono-12")
+;; (setq doom-font "SF Mono-13")
+;; (setq doom-font "SF Mono-13"
+;;      doom-variable-pitch-font "Input Mono-12")
+(setq doom-font (font-spec :family "SF Mono" :size 13 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Input Mono" :size 12 :weight 'regular))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-gruvbox)
+
+(add-to-list 'exec-path "/Users/tuomo.virolainen/bin")
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
@@ -142,7 +147,7 @@ then it takes a second \\[keyboard-quit] to abort the minibuffer."
       read-process-output-max (* 1024 1024)
       treemacs-space-between-root-nodes nil
       company-minimum-prefix-length 1
-      lsp-lens-enable nil ; Show the "1 references" etc text above definitions.
+      lsp-lens-enable t ; Show the "1 references" etc text above definitions.
       lsp-signature-auto-activate nil
       ; lsp-enable-indentation nil ; uncomment to use cider indentation instead of lsp
       ; lsp-enable-completion-at-point nil ; uncomment to use cider completion instead of lsp
