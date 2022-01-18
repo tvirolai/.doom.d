26ed3a0c0bb0549318805884a16a35b7c7983d4e
tvirolai <tuomo.virolainen@siili.com>
2022-01-18 15:21:58 +0200

    Remove extra whitespace

--- config.el
+++ config.el
@@ -210,7 +210,7 @@ In Delete Selection mode, if the mark is active, just deactivate it;
 then it takes a second \\[keyboard-quit] to abort the minibuffer."
   (interactive)
   (if (and delete-selection-mode transient-mark-mode mark-active)
-      (setq deactivate-mark  t)
+      (setq deactivate-mark t)
     (when (get-buffer "*Completions*") (delete-windows-on "*Completions*"))
     (abort-recursive-edit)))
 (define-key evil-normal-state-map [escape] 'keyboard-quit)
