4fe9f6a7ecf795d0d1f627ea371f5be91b1b2a77
Tuomo Virolainen <tuomo.virolainen@siili.com>
2022-02-25 18:03:39 +0200

    Font settings

--- config.el
+++ config.el
@@ -21,9 +21,9 @@
 ;; font string. You generally only need these two:
 ;; (setq doom-font "SF Mono-13")
 ;; (setq doom-font "SF Mono-13"
-;;      doom-variable-pitch-font "Input Mono-12")
-(setq doom-font (font-spec :family "SF Mono" :size 14 :weight 'regular)
-      doom-variable-pitch-font (font-spec :family "Fira Code" :size 13 :weight 'regular))
+;;      doom-variable-pitch-font "Input Mono-12
+(setq doom-font (font-spec :family "SF Mono" :size 13 :weight 'regular)
+      doom-variable-pitch-font (font-spec :family "Fira Code" :size 12 :weight 'regular))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
